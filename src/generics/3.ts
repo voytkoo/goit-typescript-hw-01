f6747691f0cfc32574e4f9943c16bc9bb0316d9a
olena <avoytko86@gmail.com>
2024-08-29 23:55:19 +0300

    markUp generics

--- src/generics/3.ts
+++ src/generics/3.ts
@@ -1,3 +1,6 @@
-function merge<T, U>(objA: T, objB: U): T & U {
+function merge<T extends Record<string, any>, U extends Record<string, any>>(
+  objA: T,
+  objB: U
+): T & U {
   return Object.assign({}, objA, objB);
 }
